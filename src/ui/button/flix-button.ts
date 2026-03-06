f892fefcd734d9b999973e39df5a15f75581199b
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2026-03-06 16:47:58 +0100

    fix (button): bad hack for daisyui...

--- src/ui/button/flix-button.ts
+++ src/ui/button/flix-button.ts
@@ -16,6 +16,20 @@ export class FlixButton {
   get classes(): Array<string> {
     const classes = [];
 
+    // Hack (very bad :-))) spécifique à DaisyUI (et Tailwind)
+    // Pour le forcer à générer le CSS des boutons
+    // @todo Voir sur DaisyUI/Tailwind comment forcer
+    // la génération de certaines classes même si inutilisé
+
+    // btn-primary
+    // btn-outline
+    // btn-secondary
+    // btn-xs
+    // btn-sm
+    // btn-md
+    // btn-lg
+    // btn-xl
+
     if (this.type()) classes.push(`btn-${this.type()}`);
     if (this.size()) classes.push(`btn-${this.size()}`);
 
